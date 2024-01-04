853cd64e902cc4df9425618de17de3b36f48642b
JACQUIER François <francois.jacquier@sully-group.fr>
2024-01-04 16:58:49 +0100

    add base css, base content

---
+++ src/app/[slug]/page.tsx
@@ -0,0 +1,13 @@
+
+
+export default function Post({ params }: { params: { slug: string } }) {
+
+  return <>
+    <h1>Open sans</h1>
+    <h2>SUB TITLE</h2>
+
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti quod sunt error dolorum officiis placeat dignissimos. Amet accusantium praesentium expedita quidem exercitationem, quibusdam similique laudantium, nam at earum recusandae laborum.
+    </p>
+  </>;
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,25 @@
-import Image from 'next/image';
-import styles from './page.module.css';
+import Link from "next/link";
+
+
+const DATA = [
+  {
+    bookTitle: 'Sunset limited',
+    bookAuthor: 'James Lee Burke',
+    summary: 'I had seen a dawn like this only twice in my life: once...',
+    slug: 'sunset-limited',
+  }
+]
 
 export default function Home() {
-  return <main>books</main>;
+  return <>
+    <h1>A blog of books</h1>
+    <h2>How to love to read</h2>
+
+    {DATA.map(blogPost => (
+      <article>
+        <strong><Link href={`/${blogPost.slug}`}>{blogPost.bookTitle}, <em>{blogPost.bookAuthor}</em></Link></strong>
+        <p>{blogPost.summary}</p>
+      </article>
+    ))}
+  </>;
 }
